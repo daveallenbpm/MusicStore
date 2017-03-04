9a0040f9d99c2fde279840510e2196d2a576031b
Dave Allen <daveallenbpm@gmail.com>
2017-03-04 14:07:06 +0000

    Minor refactor

--- MusicStore/Routes.fs
+++ MusicStore/Routes.fs
@@ -24,14 +24,6 @@ let hasHeader (header: string * string) (context: HttpContext) =
         | None -> None
     )
 
-type BasicResponse = {
-    message: string
-}
-
-type SerializationType =
-| Json
-| Xml
-
 type ISerializer =
     abstract member serialize: 'a -> string
 
@@ -43,40 +35,34 @@ type XmlSerializer() =
     interface ISerializer with
         member this.serialize (data: 'a) : string = "<xml><test></test></xml>"
 
-let createBasicResponse (s: SerializationType) message : string =
-    let serialize =
-        match s with
-        | Json -> (JsonSerializer() :> ISerializer).serialize
-        | Xml -> (XmlSerializer() :> ISerializer).serialize
-    serialize { message = message }
+type BasicResponse = {
+    message: string
+}
 
-let apiRoutes (serializationType: SerializationType) =
-    let mimeType =
-        match serializationType with
-        | Json -> "application/json"
-        | Xml -> "application/xml"
-        
-    let serializer =
-        match serializationType with
-        | Json -> JsonSerializer() :> ISerializer
-        | Xml -> XmlSerializer() :> ISerializer
+let createBasicResponse (s: ISerializer) message : string =
+    s.serialize{ message = message }
+
+let apiRoutes (serializer: ISerializer) (mimeType: string) =
     choose 
         [
             path "/hello" >=> GET >=> OK "Got hello successfully"
-            path "/track" >=> POST >=> hasHeader ("content-type", "application/json") >=> CREATED ((createBasicResponse serializationType "Track created"))
+            path "/track" >=> POST >=> hasHeader ("content-type", "application/json") >=> CREATED ((createBasicResponse serializer "Track created"))
             pathScan "/track/%i" (fun trackId ->
                     (ApiModel.retrieve DataLayer.getTrack trackId)
                     |> serializer.serialize
                     |> OK
                 )
             path "/eshdui" >=> GET >=> OK (serializer.serialize { message = "12345" })
-            NotFound (createBasicResponse serializationType "The resource you're looking for isn't here")
+            NotFound (createBasicResponse serializer "The resource you're looking for isn't here")
         ] >=> Writers.setMimeType (sprintf "%s; charset=utf-8" mimeType)
 
 let routes =
+    let jsonSerializer = JsonSerializer() :> ISerializer
+    let xmlSerializer = XmlSerializer() :> ISerializer
+
     choose
         [ 
-            hasHeader ("accept", "application/json") >=> (apiRoutes SerializationType.Json)
-            hasHeader ("accept", "application/xml") >=> (apiRoutes SerializationType.Xml)
-            BAD_REQUEST (createBasicResponse SerializationType.Json "Only JSON here you turd.")
+            hasHeader ("accept", "application/json") >=> (apiRoutes jsonSerializer"application/json")
+            hasHeader ("accept", "application/xml") >=> (apiRoutes xmlSerializer "application/xml")
+            BAD_REQUEST (createBasicResponse jsonSerializer "Only JSON here you turd.")
         ]
